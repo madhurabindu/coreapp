d8f6d575f771b08459f16828c008d60e007662f5
mb <madhura.bindu@gmail.com>
2020-08-15 12:36:57 +0530

    Update Program.cs
    making it work

--- CoreApp/Program.cs
+++ CoreApp/Program.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-
 namespace CoreApp
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        {    Console.WriteLine("Hello World!");   }
     }
 }
