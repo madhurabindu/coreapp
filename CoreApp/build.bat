e7923460b5a81f4b3021df8ee44d7fb9385f73db
Madhura Bindu <madhura.bindu@xymob.in>
2020-08-15 11:52:21 +0530

    build related

--- CoreApp/build.bat
+++ CoreApp/build.bat
@@ -1,8 +1,8 @@
+dotnet restore
 pushd "%~dp0"
 if exist Debug rd /s /q Debug
 if exist Release rd /s /q Release
-if exist x64 rd /s /q x64
-"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" 
+
 "%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" 
 :exit
 popd
