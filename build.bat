5078d61da0b1cb2c6bd398e648c050b267443aee
Madhura Bindu <madhura.bindu@xymob.in>
2020-08-15 12:14:31 +0530

    build

--- build.bat
+++ build.bat
@@ -4,7 +4,8 @@ if exist Debug rd /s /q Debug
 if exist Release rd /s /q Release
 
 "%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" 
+
+dotnet run -p CoreApp
+
 :exit
 popd
-@echo off
-@echo on
