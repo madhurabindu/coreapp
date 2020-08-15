7bbcfb0e6e44a466bb1a9bebc5895a5fba63b843
mb <madhura.bindu@gmail.com>
2020-08-15 12:44:14 +0530

    Update loc.bat

--- loc.bat
+++ loc.bat
@@ -3,5 +3,6 @@ set file=coreapp\program.cs
 set /a cnt=0
 for /f %%a in ('type "%file%"^|find "" /v /c') do set /a cnt=%%a
 echo %file% has %cnt% lines
-if (cnt  gtr 10) exit 0
+if (cnt LEQ 10) exit 0
+echo Modularity check failed. Max allowed lines 10
 exit 300
