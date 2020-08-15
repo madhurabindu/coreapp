f362c8507afd57328f76cf5dc0669b5b4c0d82b3
mb <madhura.bindu@gmail.com>
2020-08-15 12:30:15 +0530

    Update loc.bat

--- loc.bat
+++ loc.bat
@@ -3,4 +3,5 @@ set file=coreapp\program.cs
 set /a cnt=0
 for /f %%a in ('type "%file%"^|find "" /v /c') do set /a cnt=%%a
 echo %file% has %cnt% lines
-EXIT /B (cnt  < 10)
+if (cnt  gtr 10) exit 0
+exit 300
