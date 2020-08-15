44f660b113528b7df136fe9d21945b85dd7ec51b
mb <madhura.bindu@gmail.com>
2020-08-15 18:52:28 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 dotnet restore
-dotnet add package Microsoft.CodeAnalysis.Metrics --version 3.3.0
+dotnet add coreapp package Microsoft.CodeAnalysis.Metrics --version 3.3.0
 pushd "%~dp0"
 if exist Debug rd /s /q Debug
 if exist Release rd /s /q Release
