c16e5fb3e919c06eb649445047bdf8069a68dc61
mb <madhura.bindu@gmail.com>
2020-08-15 14:40:22 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ pushd "%~dp0"
 if exist Debug rd /s /q Debug
 if exist Release rd /s /q Release
 
-"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" 
+"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /t:Metrics
 
 dotnet run -p CoreApp
 
